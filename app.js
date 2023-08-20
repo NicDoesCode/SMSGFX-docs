d017cedaef413463c129f7e958ba494ff906317e
Nicholas Furgiuele <nicholas@distinctive.com.au>
2023-08-20 12:54:07 +1000

    Allow build locally.

---
+++ app.js
@@ -0,0 +1,60 @@
+import path from "path";
+import fs from "fs";
+import url from 'url';
+import express from 'express';
+import https from 'https';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const app = express();
+const portHttp = 8080;
+const portHttps = 8443;
+
+
+function startServer() {
+
+    let servPath = 'docs/build/html';
+
+    app.use(express.static(servPath));
+
+    console.log(`Path: ${path.join(__dirname, servPath)}`);
+    
+    app.listen(portHttp, () => {
+        console.log(`Server running at: http://localhost:${portHttp}`);
+    });
+
+    if (fs.existsSync('./cert/server.key') && fs.existsSync('./cert/server.cert')) {
+        https.createServer({
+            key: fs.readFileSync('./cert/server.key'),
+            cert: fs.readFileSync('./cert/server.cert')
+        }, app).listen(8443, () => {
+            console.log(`Server running at: https://localhost:${portHttps}`);
+        });
+    } else {
+        console.warn(`Unable to create HTTPS server on port ${portHttps} because the files './cert/server.key' and './cert/server.cert' do not exist.`);
+    }
+
+}
+
+
+/**
+ * Gets the environment from the '--path' or '-p' parameter.
+ * @returns {string}
+ */
+function getPath() {
+    let result = null;
+    process.argv.forEach((arg, index, args) => {
+        if (arg === '--path' || arg === '-p') {
+            if (index + 1 < args.length) {
+                const env = args[index + 1];
+                if (!env.startsWith('-')) {
+                    result = env;
+                }
+            }
+        }
+    });
+    return result;
+}
+
+
+startServer();
